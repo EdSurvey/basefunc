aeda1677b34a17459e5498e0a01fbd6ba0485a5e
GreStas <grestas2000@gmail.com>
2017-10-11 18:56:26 +0300

    #2 Created model Client and added in admin.

--- EdSurvey/clients/admin.py
+++ EdSurvey/clients/admin.py
@@ -1,3 +1,11 @@
+#   clients.admin
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client
+
+
+class ClientAdmin(admin.ModelAdmin):
+    model = Client
+    list_display = ('id', 'shortname', 'name')
+
+admin.site.register(Client, ClientAdmin)
--- EdSurvey/clients/apps.py
+++ EdSurvey/clients/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ClientsConfig(AppConfig):
     name = 'clients'
+    verbose_name = 'Клиенты'
