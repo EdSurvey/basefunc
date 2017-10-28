fdcd79f8267dfab10a586fb2687070871145d386
GreStas <grestas2000@gmail.com>
2017-10-28 23:05:13 +0300

    #49 Create models QueryList and QueryContent for app querylists.
    Add admin settings.

--- EdSurvey/querylists/admin.py
+++ EdSurvey/querylists/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import QueryList, QueryContent
+
+
+@admin.register(QueryList)
+class QueryListAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'division', 'public')
+
+
+@admin.register(QueryContent)
+class QueryContentAdmin(admin.ModelAdmin):
+    list_display = ('querylist', 'question', 'ordernum')
--- EdSurvey/querylists/models.py
+++ EdSurvey/querylists/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from questions.models import Question
+from clients.models import Division, Person
+
+
+class QueryList(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    division = models.ForeignKey(Division, on_delete=models.PROTECT)
+    public = models.BooleanField(default=False)
+    owner = models.ForeignKey(Person, on_delete=models.PROTECT, verbose_name='владелец')
+
+    class Meta:
+        verbose_name = 'Опросник'
+        verbose_name_plural = 'Опросники'
+
+    def __str__(self):
+        return "{}.{}".format(self.id, self.name)
+
+
+class QueryContent(models.Model):
+    querylist = models.ForeignKey(QueryList, on_delete=models.PROTECT)
+    question = models.ForeignKey(Question, on_delete=models.PROTECT)
+    ordernum = models.PositiveIntegerField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Наполнение опросника'
+        verbose_name_plural = 'Наполнение опросников'
+        unique_together = ('querylist', 'question')
+
+    def __str__(self):
+        return "{}-{}".format(self.querylist, self.question)
