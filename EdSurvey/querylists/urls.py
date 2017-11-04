ec87c6b04036b2e21a8d51b8176eced4a0b1f49f
GreStas <grestas2000@gmail.com>
2017-11-04 16:11:14 +0200

    #49 Add edit form and code for New QuesryList and Edit QueryList.

--- EdSurvey/querylists/urls.py
+++ EdSurvey/querylists/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    # url(r'^editquerylist/(?P<questionid>\d+)$', views.edit_querylist, name='editquerylist'),
-    # url(r'^newquerylist$', views.new_querylist, name='newquerylist'),
+    url(r'^editquerylist/(?P<querylistid>\d+)$', views.edit_querylist, name='editquerylist'),
+    url(r'^newquerylist$', views.new_querylist, name='newquerylist'),
     # url(r'^editqcontents/(?P<qlistid>\d+)$', views.edit_qcontents, name='editqcontents'),
     # url(r'^addqcontent/(?P<qlistid>\d+)$', views.new_qcontent, name='addqcontent'),
 ]
