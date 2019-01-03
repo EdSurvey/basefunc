6b2fac6284cec941fb308fb477abdcab37e78ce1
GreStas <grestas2000@gmail.com>
2019-01-03 17:51:38 +0200

    #64 Add edit task, work with errors

--- EdSurvey/schedules/urls.py
+++ EdSurvey/schedules/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^schedule$', views.schedule_index, name='schedule'),
     url(r'^task$', views.task_index, name='task'),
-    # url(r'^schedule/(?P<scheduleid>\d+)$', views.schedule_info, name='scheduleinfo'),
+    url(r'^edittask/(?P<taskid>\d+)$', views.edit_task, name='edittask'),
+    url(r'^newtask$', views.new_task, name='newtask'),
+    url(r'^schedule$', views.schedule_index, name='schedule'),
+    url(r'^editschedule/(?P<scheduleid>\d+)$', views.edit_schedule, name='editschedule'),
+    url(r'^addschedule/(?P<taskid>\d+)$', views.new_schedule, name='addschedule'),
 ]
