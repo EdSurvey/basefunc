a3483e902f673fb3f4f8a05406e948369d47a1f4
GreStas <grestas2000@gmail.com>
2019-01-07 18:12:32 +0200

    #69 Fix bug with show token.key in message

--- EdSurvey/clients/views.py
+++ EdSurvey/clients/views.py
@@ -1,6 +1,6 @@
 #  clients.views
 from rest_framework.authtoken.models import Token
-from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
@@ -47,9 +47,6 @@ def log_in(request):
                 touch_person(active_person)
             request.session['person_id'] = active_person.id
 
-            if request.POST.get("Token"):
-                token, created = Token.objects.get_or_create(user=user)
-                messages.add_message(request, messages.INFO, "Your's token is ".format(token.key))
             try:
                 return redirect(request.GET['next'])
             except MultiValueDictKeyError:
@@ -66,6 +63,7 @@ def log_out(request):
     return redirect(reverse('homepage'))
 
 
+@login_required(login_url='login')
 def aka(request):
     if request.method == 'POST' and request.POST.get("Ok") and request.POST.get('choice'):
         person_id = request.POST.get('choice')
@@ -74,8 +72,10 @@ def aka(request):
         request.session['person_id'] = person.id
         return redirect(reverse('homepage'))
     persons = Person.objects.filter(user=request.user)
+    token, created = Token.objects.get_or_create(user=request.user)
     return render(request,
                   'aka.html',
                   {
                       'persons': persons,
+                      'token': token,
                   },)
